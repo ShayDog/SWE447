582f00fb9a112451cdc8024c60db06a61def7d19
ShayDog <shaydes@comcast.net>
2018-01-30 11:59:39 -0800

    added cone var

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -1,10 +1,13 @@
 var gl = null;
+var cone = null;
 
 function init() {
-    var canvas = document.getElementById( "webgl-canvas" );
+   
 
     gl = WebGLUtils.setupWebGL( canvas );
-
+    
+    var cone = new Cone( gl );
+    
     if ( !gl ) {
         alert("Unable to setup WebGL");
         return;
@@ -12,7 +15,7 @@ function init() {
 
     gl.clearColor( 0.5, 0.1, 0.2, 0.5 );
 
-    render();
+    cone.render();
 }
 
 function render() {
